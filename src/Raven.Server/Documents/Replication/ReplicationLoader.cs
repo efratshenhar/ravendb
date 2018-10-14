64ae639fb74a1f8e42e357d5bb80c8221914226c
efrat@ayende.com <efrat@ayende.com>
2018-10-14 13:34:55 +0300

    a

--- src/Raven.Server/Documents/Replication/ReplicationLoader.cs
+++ src/Raven.Server/Documents/Replication/ReplicationLoader.cs
@@ -749,6 +749,7 @@ namespace Raven.Server.Documents.Replication
                     
                     using (var cts = new CancellationTokenSource((_server.Engine.TcpConnectionTimeout)*10))
                     {
+                        Console.WriteLine($"{_server.NodeTag} :{node.FromString()} ");
                         var x = ReplicationUtils.GetTcpInfo(internalNode.Url, internalNode.Database, "Replication", _server.Server.Certificate.Certificate, cts.Token);
                         Console.WriteLine($"{_server.NodeTag} :{node.FromString()} : {x.Port} : {x.Url}");
                         return x;
