d5583f42e09d448709d34e5f869deac5432de7ad
efrat@ayende.com <efrat@ayende.com>
2018-10-18 11:41:32 +0300

    a

--- src/Raven.Client/Util/TcpUtils.cs
+++ src/Raven.Client/Util/TcpUtils.cs
@@ -115,7 +115,7 @@ namespace Raven.Client.Util
             var expectedCert = new X509Certificate2(Convert.FromBase64String(info.Certificate), (string)null, X509KeyStorageFlags.MachineKeySet);
             var sslStream = new SslStream(stream, false, (sender, actualCert, chain, errors) =>
             {
-                Console.WriteLine("Get");
+                Console.WriteLine($"Get { expectedCert.Equals(actualCert)}");
                 return expectedCert.Equals(actualCert);
             });
             await sslStream.AuthenticateAsClientAsync(new Uri(info.Url).Host, new X509CertificateCollection(new X509Certificate[]{storeCertificate}), SslProtocols.Tls12, false).ConfigureAwait(false);
