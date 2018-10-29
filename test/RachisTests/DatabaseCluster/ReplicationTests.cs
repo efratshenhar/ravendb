4e291a0405b2e8620314a301e8918f06d753aefb
efrat@ayende.com <efrat@ayende.com>
2018-10-29 14:10:01 +0200

    a

--- test/RachisTests/DatabaseCluster/ReplicationTests.cs
+++ test/RachisTests/DatabaseCluster/ReplicationTests.cs
@@ -698,7 +698,7 @@ namespace RachisTests.DatabaseCluster
                     await session.StoreAsync(new User { Name = "Indych" }, "users/2");
                     await session.SaveChangesAsync();
                 }
-                await Task.Delay(200);
+                await Task.Delay(600);
                 using (var session = store.OpenAsyncSession())
                 {
                     var user = await session.LoadAsync<User>("users/2");
