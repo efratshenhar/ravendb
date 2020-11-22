dc6850d80d5b225c2722aa7c162a3e23d0eabce3
efrat@ayende.com <efrat@ayende.com>
2020-11-22 11:06:44 +0200

    WIP

--- test/SlowTests/Issues/RavenDB-15625.cs
+++ test/SlowTests/Issues/RavenDB-15625.cs
@@ -9,6 +9,7 @@ using Raven.Client.Util;
 using Raven.Server.Config;
 using Raven.Server.Documents.Indexes;
 using Raven.Server.Documents.Indexes.Auto;
+using SlowTests.Core.Utils.Entities;
 using Tests.Infrastructure;
 using Xunit;
 using Xunit.Abstractions;
@@ -22,7 +23,7 @@ namespace SlowTests.Issues
         }
 
         [Fact]
-        public async Task NotInRehabWithDisabledIndexes()
+        public async Task NotInRehabWithDisabledIndexes1()
         {
             var settings = new Dictionary<string, string>()
             {
@@ -43,7 +44,7 @@ namespace SlowTests.Issues
                 AsyncHelpers.RunSync(() => documentDatabase.IndexStore.CreateIndex(definition, Guid.NewGuid().ToString()));
                 WaitForIndexingInTheCluster(store);
                 foreach (var server in Servers)
-                {
+                { 
                     documentDatabase = await server.ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
                     var index = documentDatabase.IndexStore.GetIndex(indexName);
                     index.SetState(IndexState.Disabled);
@@ -71,7 +72,7 @@ namespace SlowTests.Issues
         }
 
         [Fact]
-        public async Task StayInRehabWithDisabledIndexes()
+        public async Task NotInRehabWithDisabledIndexes2()
         {
             var settings = new Dictionary<string, string>()
             {
@@ -104,15 +105,63 @@ namespace SlowTests.Issues
                 var val = await WaitForValueAsync(async () => await GetMembersCount(store, database), 2);
                 Assert.Equal(2, val);
 
-                await Task.Delay(TimeSpan.FromSeconds(10));
+                val = await WaitForValueAsync(async () => await GetMembersCount(store, store.Database), 3);
+                Assert.Equal(3, val);
 
-                val = await WaitForValueAsync(async () => await GetMembersCount(store, store.Database), 2);
-                Assert.Equal(2, val);
-
-                rehabs = await WaitForValueAsync(async () => await GetRehabCount(store, store.Database), 1);
-                Assert.Equal(1, rehabs);
+                rehabs = await WaitForValueAsync(async () => await GetRehabCount(store, store.Database), 0);
+                Assert.Equal(0, rehabs);
             }
         }
+
+        [Fact]
+        public async Task NotInRehabWithDisabledIndexes3()
+        {
+            var settings = new Dictionary<string, string>()
+            {
+                [RavenConfiguration.GetKey(x => x.Cluster.MoveToRehabGraceTime)] = "5"
+            };
+            var leader = await CreateRaftClusterAndGetLeader(3, customSettings: settings);
+            var database = GetDatabaseName();
+            await CreateDatabaseInClusterInner(new DatabaseRecord(database), 3, leader.WebUrl, null);
+            using (var store = new DocumentStore
+            {
+                Database = database,
+                Urls = new[] { leader.WebUrl }
+            }.Initialize())
+            {
+                var documentDatabase = await Servers[2].ServerStore.DatabasesLandlord.TryGetOrCreateResourceStore(database);
+                var definition = new AutoMapIndexDefinition("Users", new[] { new AutoIndexField { Name = "Name", Storage = FieldStorage.No }, });
+                string indexName = definition.Name;
+                AsyncHelpers.RunSync(() => documentDatabase.IndexStore.CreateIndex(definition, Guid.NewGuid().ToString()));
+                var index = documentDatabase.IndexStore.GetIndex(indexName);
+                index.SetState(IndexState.Disabled);
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(new User(){Name = "Toli"}, "user/1");
+                    session.SaveChanges();
+
+                }
+                
+                var record = await store.Maintenance.Server.SendAsync(new GetDatabaseRecordOperation(store.Database));
+                record.Topology.Members.Remove(Servers[2].ServerStore.NodeTag);
+                record.Topology.Rehabs.Add(Servers[2].ServerStore.NodeTag);
+                await store.Maintenance.Server.SendAsync(new UpdateDatabaseOperation(record, record.Etag));
+
+                var rehabs = await WaitForValueAsync(async () => await GetRehabCount(store, store.Database), 1);
+                Assert.Equal(1, rehabs);
+
+                var val = await WaitForValueAsync(async () => await GetMembersCount(store, database), 2);
+                Assert.Equal(2, val);
+
+                val = await WaitForValueAsync(async () => await GetMembersCount(store, store.Database), 3);
+                Assert.Equal(3, val);
+
+                rehabs = await WaitForValueAsync(async () => await GetRehabCount(store, store.Database), 0);
+                Assert.Equal(0, rehabs);
+            }
+        }
+
 
         private static async Task<int> GetMembersCount(IDocumentStore store, string databaseName)
         {
